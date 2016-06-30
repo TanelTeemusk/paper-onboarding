209c0e22c2d0b648841ed14ca423567db69522cc
Tanel Teemusk <tanel@teemusk.com>
2016-06-30 13:06:22 +0300

    Onboarding updates

--- Source/PageView/PageContainerView/PageContainer.swift
+++ Source/PageView/PageContainerView/PageContainer.swift
@@ -9,125 +9,132 @@
 import UIKit
 
 class PageContrainer: UIView {
-  
-  var items: [PageViewItem]?
-  let space: CGFloat // space between items
-  var currentIndex = 0
-  
-  private let itemRadius: CGFloat
-  private let selectedItemRadius: CGFloat
-  private let itemsCount: Int
-  private let animationKey = "animationKey"
-  
-  init(radius: CGFloat, selectedRadius: CGFloat, space: CGFloat, itemsCount: Int) {
-    self.itemsCount         = itemsCount
-    self.space              = space
-    self.itemRadius         = radius
-    self.selectedItemRadius = selectedRadius
-    super.init(frame: CGRect.zero)
-    items = createItems(itemsCount, radius: radius, selectedRadius: selectedRadius)
-  }
-  
-  required init?(coder aDecoder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
+    
+    var items: [PageViewItem]?
+    let space: CGFloat // space between items
+    var currentIndex = 0
+    
+    private let itemRadius: CGFloat
+    private let selectedItemRadius: CGFloat
+    private let borderColor: UIColor
+    private let selectedBorderColor:UIColor
+    private let itemsCount: Int
+    private let animationKey = "animationKey"
+    
+    init(radius: CGFloat, selectedRadius: CGFloat, space: CGFloat, itemsCount: Int, borderColor:UIColor, selectedBorderColor:UIColor) {
+        
+        self.itemsCount         = itemsCount
+        self.space              = space
+        self.itemRadius         = radius
+        self.selectedItemRadius = selectedRadius
+        self.borderColor = borderColor
+        self.selectedBorderColor = selectedBorderColor
+        super.init(frame: CGRect.zero)
+        items = createItems(itemsCount, radius: radius, selectedRadius: selectedRadius)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
 // MARK: public
 
 extension PageContrainer {
-  
-  func currenteIndex(index: Int, duration: Double, animated: Bool) {
-    guard let items = self.items where
-      index != currentIndex else {return}
     
-    animationItem(items[index], selected: true, duration: duration)
-    
-    let fillColor = index > currentIndex ? true : false
-    animationItem(items[currentIndex], selected: false, duration: duration, fillColor: fillColor)
-    
-    currentIndex = index
-  }
+    func currenteIndex(index: Int, duration: Double, animated: Bool) {
+        guard let items = self.items where
+            index != currentIndex else {return}
+        
+        animationItem(items[index], selected: true, duration: duration)
+        
+        let fillColor = index > currentIndex ? true : false
+        animationItem(items[currentIndex], selected: false, duration: duration, fillColor: fillColor)
+        
+        currentIndex = index
+    }
 }
 
-// MARK: animations 
+// MARK: animations
 
 extension PageContrainer {
-  
-  private func animationItem(item: PageViewItem, selected: Bool, duration: Double, fillColor: Bool = false) {
-    let toValue = selected == true ? selectedItemRadius * 2 : itemRadius * 2
-      item.constraints
-        .filter{ $0.identifier == "animationKey" }
-        .forEach {
-          $0.constant = toValue
-      }
     
-    UIView.animateWithDuration(duration, delay: 0, options: .CurveEaseOut, animations: {
-      self.layoutIfNeeded()
-    }, completion: nil)
-    
-    item.animationSelected(selected, duration: duration, fillColor: fillColor)
-  }
+    private func animationItem(item: PageViewItem, selected: Bool, duration: Double, fillColor: Bool = false) {
+        let toValue = selected == true ? selectedItemRadius * 2 : itemRadius * 2
+        item.constraints
+            .filter{ $0.identifier == "animationKey" }
+            .forEach {
+                $0.constant = toValue
+        }
+        
+        UIView.animateWithDuration(duration, delay: 0, options: .CurveEaseOut, animations: {
+            self.layoutIfNeeded()
+            }, completion: nil)
+        item.animationSelected(selected, duration: duration, fillColor: fillColor)
+    }
 }
 // MARK: create
 
 extension PageContrainer {
-  
-  private func createItems(count: Int, radius: CGFloat, selectedRadius: CGFloat) -> [PageViewItem] {
-    var items = [PageViewItem]()
-    // create first item 
-    var item = createItem(radius, selectedRadius: selectedRadius, isSelect: true)
-    addConstraintsToView(item, radius: selectedRadius)
-    items.append(item)
     
-    for _ in 1..<count {
-      let nextItem = createItem(radius, selectedRadius: selectedRadius)
-      addConstraintsToView(nextItem, leftItem: item, radius: radius)
-      items.append(nextItem)
-      item = nextItem
-    }
-    return items
-  }
-  
-  private func createItem(radius: CGFloat, selectedRadius: CGFloat, isSelect: Bool = false) -> PageViewItem {
-    let item = Init(PageViewItem(radius: radius, selectedRadius: selectedRadius, isSelect: isSelect)) {
-      $0.translatesAutoresizingMaskIntoConstraints = false
-      $0.backgroundColor                           = .clearColor()
-    }
-    self.addSubview(item)
-    
-    return item
-  }
-  
-  private func addConstraintsToView(item: UIView, radius: CGFloat) {
-    [NSLayoutAttribute.Left, NSLayoutAttribute.CenterY].forEach { attribute in
-      (self, item) >>>- { $0.attribute = attribute }
+    private func createItems(count: Int, radius: CGFloat, selectedRadius: CGFloat) -> [PageViewItem] {
+        var items = [PageViewItem]()
+        // create first item
+        var item = createItem(radius, selectedRadius: selectedRadius, isSelect: true)
+        addConstraintsToView(item, radius: selectedRadius)
+        items.append(item)
+        
+        for _ in 1..<count {
+            let nextItem = createItem(radius, selectedRadius: selectedRadius)
+            addConstraintsToView(nextItem, leftItem: item, radius: radius)
+            items.append(nextItem)
+            item = nextItem
+        }
+        return items
     }
     
-    [NSLayoutAttribute.Width, NSLayoutAttribute.Height].forEach { attribute in
-      item >>>- {
-        $0.attribute  = attribute
-        $0.constant   = radius * 2.0
-        $0.identifier = animationKey
-      } 
-    }
-  }
-  
-  private func addConstraintsToView(item: UIView, leftItem: UIView, radius: CGFloat) {
-    (self, item) >>>- { $0.attribute = .CenterY }
-    
-    (self, item, leftItem) >>>- {
-      $0.attribute       = .Leading
-      $0.secondAttribute = .Trailing
-      $0.constant        = space
+    private func createItem(radius: CGFloat, selectedRadius: CGFloat, isSelect: Bool = false) -> PageViewItem {
+        let item = Init(PageViewItem(radius: radius, selectedRadius: selectedRadius, isSelect: isSelect, borderColor:self.borderColor, selectedBorderColor:self.selectedBorderColor)) {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.backgroundColor                           = .clearColor()
+        }
+        if isSelect {
+            item.animationSelected(isSelect, duration: 0.1, fillColor: false)
+        }
+        self.addSubview(item)
+        
+        return item
     }
     
-    [NSLayoutAttribute.Width, NSLayoutAttribute.Height].forEach { attribute in
-      item >>>- {
-        $0.attribute  = attribute
-        $0.constant   = radius * 2.0
-        $0.identifier = animationKey
-      } 
+    private func addConstraintsToView(item: UIView, radius: CGFloat) {
+        [NSLayoutAttribute.Left, NSLayoutAttribute.CenterY].forEach { attribute in
+            (self, item) >>>- { $0.attribute = attribute }
+        }
+        
+        [NSLayoutAttribute.Width, NSLayoutAttribute.Height].forEach { attribute in
+            item >>>- {
+                $0.attribute  = attribute
+                $0.constant   = radius * 2.0
+                $0.identifier = animationKey
+            }
+        }
     }
-  }
+    
+    private func addConstraintsToView(item: UIView, leftItem: UIView, radius: CGFloat) {
+        (self, item) >>>- { $0.attribute = .CenterY }
+        
+        (self, item, leftItem) >>>- {
+            $0.attribute       = .Leading
+            $0.secondAttribute = .Trailing
+            $0.constant        = space
+        }
+        
+        [NSLayoutAttribute.Width, NSLayoutAttribute.Height].forEach { attribute in
+            item >>>- {
+                $0.attribute  = attribute
+                $0.constant   = radius * 2.0
+                $0.identifier = animationKey
+            } 
+        }
+    }
 }
--- Source/PageView/PageView.swift
+++ Source/PageView/PageView.swift
@@ -9,160 +9,165 @@
 import UIKit
 
 class PageView: UIView {
-  
-  var itemsCount                  = 3
-  var itemRadius: CGFloat         = 8.0
-  var selectedItemRadius: CGFloat = 22.0
-  var duration: Double            = 0.7
-  var space: CGFloat              = 20// space between items
-
-  // configure items set image or chage color for border view
-  var configuration: ((item: PageViewItem, index: Int) -> Void)? {
-    didSet {
-      configurePageItems(containerView?.items)
+    
+    private var itemsCount                  = 0
+    private var itemRadius: CGFloat         = 0
+    private var selectedItemRadius: CGFloat = 0
+    private var duration: Double            = 0.7
+    private var space: CGFloat              = 10// space between items
+    
+    var borderColor: UIColor = UIColor.whiteColor()
+    var selectedBorderColor:UIColor = UIColor.whiteColor()
+    
+    // configure items set image or chage color for border view
+    var configuration: ((item: PageViewItem, index: Int) -> Void)? {
+        didSet {
+            configurePageItems(containerView?.items)
+        }
     }
-  }
-  
-  private var containerX: NSLayoutConstraint?
-  private var containerView: PageContrainer?
-  
-  override init(frame: CGRect) {
-    super.init(frame: frame)
-    commonInit()
-  }
-  
-  init(frame: CGRect, itemsCount: Int, radius: CGFloat, selectedRadius: CGFloat) {
-    self.itemsCount         = itemsCount
-    self.itemRadius         = radius
-    self.selectedItemRadius = selectedRadius
-    super.init(frame: frame)
-    commonInit()
-  }
-  
-  required init?(coder aDecoder: NSCoder) {
-    super.init(coder: aDecoder)
-    commonInit()
-  }
+    
+    private var containerX: NSLayoutConstraint?
+    private var containerView: PageContrainer?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    init(frame: CGRect, itemsCount: Int, radius: CGFloat, selectedRadius: CGFloat, borderColor:UIColor, selectedBorderColor:UIColor) {
+        self.itemsCount         = itemsCount
+        self.itemRadius         = radius
+        self.selectedItemRadius = selectedRadius
+        self.borderColor = borderColor
+        self.selectedBorderColor = selectedBorderColor
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
 }
 
 // MARK: public
 
 extension PageView {
-  
-  class func pageViewOnView(view: UIView, itemsCount: Int, bottomConstant: CGFloat, radius: CGFloat, selectedRadius: CGFloat) -> PageView {
-   let pageView = Init(PageView(frame: CGRect.zero, itemsCount: itemsCount, radius: radius, selectedRadius: selectedRadius)) {
-      $0.translatesAutoresizingMaskIntoConstraints = false
-      $0.alpha                                     = 0.4
-    }
-    view.addSubview(pageView)
     
-    // add constraints 
-    for (attribute, const) in [(NSLayoutAttribute.Left, 0), (NSLayoutAttribute.Right, 0), (NSLayoutAttribute.Bottom, bottomConstant)] {
-      (view, pageView) >>>- {
-        $0.constant  = CGFloat(const)
-        $0.attribute = attribute
-      }
+    class func pageViewOnView(view: UIView, itemsCount: Int, bottomConstant: CGFloat, radius: CGFloat, selectedRadius: CGFloat, borderColor:UIColor, selectedBorderColor:UIColor) -> PageView {
+        let pageView = Init(PageView(frame: CGRect.zero, itemsCount: itemsCount, radius: radius, selectedRadius: selectedRadius, borderColor: borderColor, selectedBorderColor: selectedBorderColor)) {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.alpha                                     = 1
+        }
+        view.addSubview(pageView)
+        
+        // add constraints
+        for (attribute, const) in [(NSLayoutAttribute.Left, 0), (NSLayoutAttribute.Right, 0), (NSLayoutAttribute.Bottom, bottomConstant)] {
+            (view, pageView) >>>- {
+                $0.constant  = CGFloat(const)
+                $0.attribute = attribute
+            }
+        }
+        
+        pageView >>>- {
+            $0.attribute = .Height
+            $0.constant  = 30
+        }
+        
+        return pageView
     }
     
-    pageView >>>- {
-      $0.attribute = .Height
-      $0.constant  = 30
+    func currentIndex(index: Int, animated: Bool) {
+        
+        if 0..<itemsCount ~= index {
+            containerView?.currenteIndex(index, duration: duration * 0.5, animated: animated)
+            moveContainerTo(index, animated: animated, duration: duration)
+        }
     }
     
-    return pageView
-  }
-
-  func currentIndex(index: Int, animated: Bool) {
-    
-    if 0..<itemsCount ~= index {
-      containerView?.currenteIndex(index, duration: duration * 0.5, animated: animated)
-      moveContainerTo(index, animated: animated, duration: duration)
+    func positionItemIndex(index: Int, onView: UIView) -> CGPoint? {
+        if 0..<itemsCount ~= index {
+            if let currentItem = containerView?.items?[index].imageView {
+                let pos = currentItem.convertPoint(currentItem.center, toView: onView)
+                return pos
+            }
+        }
+        return nil
     }
-  }
-  
-  func positionItemIndex(index: Int, onView: UIView) -> CGPoint? {
-    if 0..<itemsCount ~= index {
-      if let currentItem = containerView?.items?[index].imageView {
-        let pos = currentItem.convertPoint(currentItem.center, toView: onView)
-        return pos
-      }
-    }
-    return nil
-  }
 }
 
 // MARK: life cicle
 
 extension PageView {
-  
-  private func commonInit() {
-    containerView = createContainerView()
-    currentIndex(0, animated: false)
-    self.backgroundColor = .clearColor()
-  }
+    
+    private func commonInit() {
+        containerView = createContainerView()
+        currentIndex(0, animated: false)
+        self.backgroundColor = .clearColor()
+    }
 }
 
 // MARK: create
 
 extension PageView {
-  
-  private func createContainerView() -> PageContrainer {
-    let container = Init(PageContrainer(radius: itemRadius, selectedRadius: selectedItemRadius, space: space, itemsCount: itemsCount)) {
-      $0.backgroundColor                           = .clearColor()
-      $0.translatesAutoresizingMaskIntoConstraints = false
-    }
-    self.addSubview(container)
     
-    // add constraints 
-    for attribute in [NSLayoutAttribute.Top, NSLayoutAttribute.Bottom] {
-      (self, container) >>>- { $0.attribute = attribute }
+    private func createContainerView() -> PageContrainer {
+        let container = Init(PageContrainer(radius: itemRadius, selectedRadius: selectedItemRadius, space: space, itemsCount: itemsCount, borderColor:self.borderColor, selectedBorderColor:self.selectedBorderColor)) {
+            $0.backgroundColor                           = .clearColor()
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        self.addSubview(container)
+        
+        // add constraints
+        for attribute in [NSLayoutAttribute.Top, NSLayoutAttribute.Bottom] {
+            (self, container) >>>- { $0.attribute = attribute }
+        }
+        
+        containerX = (self, container) >>>- { $0.attribute = .CenterX }
+        
+        container >>>- {
+            $0.attribute = .Width
+            $0.constant  = selectedItemRadius * 2 + CGFloat(itemsCount - 1) * (itemRadius * 2) + space * CGFloat(itemsCount - 1)
+        }
+        
+        return container
     }
     
-    containerX = (self, container) >>>- { $0.attribute = .CenterX }
-    
-    container >>>- {
-      $0.attribute = .Width
-      $0.constant  = selectedItemRadius * 2 + CGFloat(itemsCount - 1) * (itemRadius * 2) + space * CGFloat(itemsCount - 1)
+    private func configurePageItems(items: [PageViewItem]?) {
+        guard let items = items else {
+            return
+        }
+        for index in 0..<items.count {
+            configuration?(item: items[index], index: index)
+        }
     }
     
-    return container
-  }
-  
-  private func configurePageItems(items: [PageViewItem]?) {
-    guard let items = items else {
-      return
-    }
-    for index in 0..<items.count {
-      configuration?(item: items[index], index: index)
-    }
-  }
-
 }
 
 // MARK: animation
 
 extension PageView {
-  
-  private func moveContainerTo(index: Int, animated: Bool = true, duration: Double = 0) {
-    guard let containerX = self.containerX else {
-      return
-    }
     
-    let containerWidth  = CGFloat(itemsCount + 1) * selectedItemRadius + space * CGFloat(itemsCount - 1)
-    let toValue         = containerWidth / 2.0 - selectedItemRadius - (selectedItemRadius + space) * CGFloat(index)
-    containerX.constant = toValue
-    
-    if animated == true {
-      UIView.animateWithDuration(duration,
-                                 delay: 0,
-                                 options: .CurveEaseInOut,
-                                 animations: {
-                                  self.layoutIfNeeded()
-                                 },
-                                 completion: nil)
-    } else {
-      layoutIfNeeded()
+    private func moveContainerTo(index: Int, animated: Bool = true, duration: Double = 0) {
+        guard let containerX = self.containerX else {
+            return
+        }
+        
+        let containerWidth  = CGFloat(itemsCount + 1) * selectedItemRadius + space * CGFloat(itemsCount - 1)
+        let toValue         = containerWidth / 2.0 - selectedItemRadius - (selectedItemRadius + space) * CGFloat(index)
+        containerX.constant = toValue
+        
+        if animated == true {
+            UIView.animateWithDuration(duration,
+                                       delay: 0,
+                                       options: .CurveEaseInOut,
+                                       animations: {
+                                        self.layoutIfNeeded()
+                },
+                                       completion: nil)
+        } else {
+            layoutIfNeeded()
+        }
     }
-  }
 }
  
